f7a4d75653fa166e4cdee619f3cfd5baf6ee2b33
Jacob Czawlytko <JCzawlytko@CHESAPEAKECONSE.local>
2021-06-22 18:01:29 -0400

    try nondaemon fix

--- tc/TC_LU_Submodule_v1.py
+++ tc/TC_LU_Submodule_v1.py
@@ -221,19 +221,41 @@ def run_trees_over_submodule(NUM_CPUS, cf):
 """
 Got these from: https://stackoverflow.com/questions/6974695/python-process-pool-non-daemonic
 """
+# class NoDaemonProcess(mp.Process):
+#     # make 'daemon' attribute always return False
+#     def _get_daemon(self):
+#         return False
+#     def _set_daemon(self, value):
+#         pass
+#     daemon = property(_get_daemon, _set_daemon)
+
+# # We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
+# # because the latter is only a wrapper function, not a proper class.
+# class MyPool(mp.pool.Pool):
+#     freeze_support()
+#     Process = NoDaemonProcess
+
+
 class NoDaemonProcess(mp.Process):
-    # make 'daemon' attribute always return False
-    def _get_daemon(self):
+    @property
+    def daemon(self):
         return False
-    def _set_daemon(self, value):
+
+    @daemon.setter
+    def daemon(self, value):
         pass
-    daemon = property(_get_daemon, _set_daemon)
+
+class NoDaemonContext(type(mp.get_context())):
+    Process = NoDaemonProcess
 
 # We sub-class multiprocessing.pool.Pool instead of multiprocessing.Pool
 # because the latter is only a wrapper function, not a proper class.
-class MyPool(mp.pool.Pool):
-    freeze_support()
-    Process = NoDaemonProcess
+class NestablePool(mp.pool.Pool):
+    def __init__(self, *args, **kwargs):
+        kwargs['context'] = NoDaemonContext()
+        super(NestablePool, self).__init__(*args, **kwargs)
+
+
 
 #########################################################################################
 #################################RUN WORKFLOWS###########################################
